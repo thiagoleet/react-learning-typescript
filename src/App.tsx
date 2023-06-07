18797912aecadc591f8be05de253a48045ba971a
Thiago Leite <thiago.leite@agileengine.com>
2023-06-06 23:11:39 -0300

    - using Forms in TS - using refs in TS - using Function props - using state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string): void => {
+    const newTodo = new Todo(text);
+
+    setTodos((prevState) => {
+      return prevState.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
