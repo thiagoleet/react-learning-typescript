ef6e420197fced8b10e6ace688186a51c771d6e8
Thiago Leite <thiago.leite@agileengine.com>
2023-06-06 23:37:21 -0300

    using Context API with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (text: string): void => {
-    const newTodo = new Todo(text);
-
-    setTodos((prevState) => {
-      return prevState.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string): void => {
-    setTodos((prevState) => {
-      return prevState.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
