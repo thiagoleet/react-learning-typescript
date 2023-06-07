af3f10f1787b23acea35a6e072351e5fc32fed2a
Thiago Leite <thiago.leite@agileengine.com>
2023-06-06 22:49:11 -0300

    Adding a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from "react";
 import "./App.css";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [
+    new Todo('Learn React'),
+    new Todo('Learn Typescript')
+  ]
+
   return (
     <div>
-
+      <Todos items={todos} />
     </div>
   );
 }
