5ef77e80ef59784f98f3dcfd80f3d4cff60c1218
Thiago Leite <thiago.leite@agileengine.com>
2023-06-06 23:16:33 -0300

    Adding styles

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -16,7 +17,7 @@ const NewTodo: React.FC<{onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,10 @@
 import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
